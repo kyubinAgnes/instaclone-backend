2ce2ba9bc4ba2815f7f54b744b5aae48e043deef
김규빈 <kyubin@gimgyubin-ui-MacBookAir.local>
2024-04-13 23:19:09 +0900

    prisma

--- server.js
+++ server.js
@@ -1,32 +1,42 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
 
 const typeDefs = `
   type Movie {
-    id: Int
-    title: String
-    year: Int
+    id: Int!
+    title: String!
+    year: Int!
+    genre: String
+    createdAt: String!
+    updatedAt: String!
   }
   type Query {
     movies: [Movie]
     movie: Movie
   }
   type Mutation {
-    createMovie(title: String!): Boolean
+    createMovie(title: String!, year: Int!, genre: String): Movie
     deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    movies: () => [],
+    movies: () => client.movie.findMany(),
     movie: () => ({ title: "Hello", year: 2024 }),
   },
   Mutation: {
-    createMovie: (_, { title }) => {
-      console.log(title);
-      return true;
-    },
+    createMovie: (_, { title, year, genre }) =>
+      client.movie.create({
+        data: {
+          title,
+          year,
+          genre,
+        },
+      }),
     deleteMovie: (_, { title }) => {
       console.log(title);
       return true;
