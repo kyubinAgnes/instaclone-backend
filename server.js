071cca7c276512a2108f488e908be80c90c5628e
김규빈 <kyubin@gimgyubin-ui-MacBookAir.local>
2024-04-13 21:50:07 +0900

    connect DB

--- server.js
+++ server.js
@@ -2,14 +2,35 @@ import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
 const typeDefs = `
+  type Movie {
+    id: Int
+    title: String
+    year: Int
+  }
   type Query {
-    hello: String
+    movies: [Movie]
+    movie: Movie
+  }
+  type Mutation {
+    createMovie(title: String!): Boolean
+    deleteMovie(title: String!): Boolean
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "test",
+    movies: () => [],
+    movie: () => ({ title: "Hello", year: 2024 }),
+  },
+  Mutation: {
+    createMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
+    deleteMovie: (_, { title }) => {
+      console.log(title);
+      return true;
+    },
   },
 };
 const server = new ApolloServer({
