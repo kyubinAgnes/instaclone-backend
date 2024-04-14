d5a068314d0330cdd12f0649bceb03dbf4a6732e
김규빈 <kyubin@gimgyubin-ui-MacBookAir.local>
2024-04-14 22:00:25 +0900

    prisma studio

--- server.js
+++ server.js
@@ -19,14 +19,15 @@ const typeDefs = `
   }
   type Mutation {
     createMovie(title: String!, year: Int!, genre: String): Movie
-    deleteMovie(title: String!): Boolean
+    deleteMovie(id: Int!): Movie
+    updateMovie(id: Int!, year: Int!): Movie
   }
 `;
 
 const resolvers = {
   Query: {
     movies: () => client.movie.findMany(),
-    movie: () => ({ title: "Hello", year: 2024 }),
+    movie: (_, { id }) => client.movie.findUnique({ where: { id } }),
   },
   Mutation: {
     createMovie: (_, { title, year, genre }) =>
@@ -37,10 +38,9 @@ const resolvers = {
           genre,
         },
       }),
-    deleteMovie: (_, { title }) => {
-      console.log(title);
-      return true;
-    },
+    deleteMovie: (_, { id }) => client.movie.delete({ where: { id } }),
+    updateMovie: (_, { id, year }) =>
+      client.movie.update({ where: { id }, data: { year } }),
   },
 };
 const server = new ApolloServer({
